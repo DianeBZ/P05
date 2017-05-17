ce1ddc9913c77e5250ef3d2682aa421b2d0d7221
vicbl <victor.blanchard@telecom-st-etienne.fr>
2017-05-17 18:59:04 +0200

    Ajout images page Manifeste et Pricing

--- RMOpportunities/app/Pricing.js
+++ RMOpportunities/app/Pricing.js
@@ -3,88 +3,144 @@ import { Text, Image, StyleSheet, View, ScrollView, Dimensions, TouchableHighlig
 
 import Translation from './Translation';
 import Contact from './Contact';
+import LinearGradient from 'react-native-linear-gradient';
 
 var width = Dimensions.get('window').width;
 var height = Dimensions.get('window').height;
 
 export default class Pricing extends Component{
- render(){
-		 BackAndroid.addEventListener('Back',this.onBackAndroid);
-		 return(
-				<ScrollView style={styles.scrollView}>
-					<Text style={styles.titlePricing}>{Translation[lang].pricing}</Text>
-					{(() => {
-						if(lang=='fr')
-							return(
-									<Image source={require('../img/Pricing_fr.jpg')} style={styles.imagePricing} />
-								  );
-						else
-							return(
-									<Image source={require('../img/Pricing_en.jpg')} style={styles.imagePricing} />
-								  );
-					})()}
-					<View style={styles.view}>
-						<TouchableHighlight onPress={this._onPressContact} style={{width: width*0.5,backgroundColor:'#A4D04A', justifyContent:'center', borderRadius:5,}} underlayColor="rgb(138,183,46)">
-							<Text style={styles.buttonText}>{Translation[lang].contactUs}</Text>
-						</TouchableHighlight>
-					</View>
-					
-					<Text />
-					<Text />
-				</ScrollView>
-				);
-		}
- onBackAndroid = () => {
-	const { navigator } = this.props;
-	if (navigator && navigator.getCurrentRoutes().length > 1) {
-		BackAndroid.removeEventListener('Back', this.onBackAndroid);
-		navigator.pop();
-		return true;
-	} else {
-		return false;
-	}
- };
- 
-  _onPressContact = () => {
-	BackAndroid.removeEventListener('Back', this.onBackAndroid);
-	const { navigator } = this.props;
-    if (navigator) {
-      navigator.push({
-        name: 'Contact',
-        component: Contact,
-      })
-    }
- };
+   render(){
+      BackAndroid.addEventListener('Back',this.onBackAndroid);
+      return(
+         <View style={styles.container}>
+            <View style={styles.header}>
+               <LinearGradient
+                  start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}
+                  locations={[0,1.0]}
+                  colors={['#185177', '#F6D092']}
+                  style={styles.gradient}>
+                  <View style={styles.headerIcon}>
+                     <Image source={require('../img/invoice.png')} style = {styles.logoLeft} />
+                  </View>
+                  <Text style={styles.title}>
+                     {Translation[lang].pricing}
+                  </Text>
+                  <View style={styles.headerIcon}>
+                     <Image source={require('../img/medal.png')} style = {styles.logoRight} />
+                  </View>
+               </LinearGradient>
+            </View>
+            <ScrollView >
+               <View style={styles.containerMain}>
+
+
+                  {(() => {
+                     if(lang==='fr')
+                     return(
+                        <View style={styles.containerMain}>
+                           <Image source={require('../img/Pricing_fr.jpg')} style={styles.imagePricing} />
+                        </View>
+                     );
+                     else
+                     return(
+                        <View style={styles.containerMain}>
+                           <Image source={require('../img/Pricing_en.jpg')} style={styles.imagePricing} />
+                        </View>
+                     );
+                  })()}
+
+                  <TouchableHighlight onPress={this._onPressContact} style={{width: width*0.5,backgroundColor:'#A4D04A', justifyContent:'center', borderRadius:5,}} underlayColor="rgb(138,183,46)">
+                     <Text style={styles.buttonText}>{Translation[lang].contactUs}</Text>
+                  </TouchableHighlight>
+               </View>
+
+
+
+            </ScrollView>
+         </View>
+      );
+   }
+   onBackAndroid = () => {
+      const { navigator } = this.props;
+      if (navigator && navigator.getCurrentRoutes().length > 1) {
+         BackAndroid.removeEventListener('Back', this.onBackAndroid);
+         navigator.pop();
+         return true;
+      } else {
+         return false;
+      }
+   };
+
+   _onPressContact = () => {
+      BackAndroid.removeEventListener('Back', this.onBackAndroid);
+      const { navigator } = this.props;
+      if (navigator) {
+         navigator.push({
+            name: 'Contact',
+            component: Contact,
+         })
+      }
+   };
 }
 
 const styles = StyleSheet.create({
-	scrollView:{
-		height:height,
-		width:width,
-		backgroundColor:'#f2f2f2',
-	},
-	view:{
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-        margin: 10,
-	},
-	imagePricing:{
-		height:1230,
-		width:width,
-	},
-	buttonText:{
-        color: "white",
-        textAlign: "center",
-        fontSize: 20,
-		alignItems: 'center',
-		alignSelf: 'center',
-    },
-	titlePricing:{
-		fontSize:32,
-		marginTop:width*0.2,
-		marginBottom:width*0.05,
-		textAlign:'center',
-       color: '#000000',
-	},
+   container: {
+      marginTop:height*0.08,
+      flex:1,
+   },
+   gradient:{
+      width: width,
+      flexDirection:'row',
+      paddingTop:15,
+      paddingBottom:15
+   },
+   header: {
+      alignItems: 'center',
+      marginBottom:15,
+   },
+   headerIcon: {
+      width:width/4,
+      alignItems:'center',
+      alignSelf:'center',
+   },
+   logoLeft:{
+      transform: [{ rotate: '-25deg'}],
+      width: 70,
+      height: 70,
+   },
+   title: {
+      width:width/2,
+      fontSize: 25,
+      textAlign: 'center',
+      color: '#333333',
+   },
+   logoRight:{
+      width: 70,
+      height: 70,
+   },
+   containerMain: {
+      width: width,
+      justifyContent:'center',
+      alignItems:'center',
+      marginRight:10,
+      marginBottom:15,
+   },
+   imagePricing:{
+      height:1230,
+      width:width,
+   },
+   buttonText:{
+      color: "white",
+      textAlign: "center",
+      fontSize: 20,
+      alignItems: 'center',
+      alignSelf: 'center',
+   },
+   titlePricing:{
+      fontSize:32,
+      marginTop:width*0.2,
+      marginBottom:width*0.05,
+      textAlign:'center',
+      color: '#000000',
+   },
 });
