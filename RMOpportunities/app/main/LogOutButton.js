064d7983d5d627f139ab72d536e05a8a3ba20f6b
vicbl <victor.blanchard@telecom-st-etienne.fr>
2017-06-01 11:22:05 +0200

    Changmenet echelle image déconnexion

--- RMOpportunities/app/main/LogOutButton.js
+++ RMOpportunities/app/main/LogOutButton.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableHighlight,
-  Dimensions,
-  Picker,
-  Alert,
-  DeviceEventEmitter,
+   StyleSheet,
+   Text,
+   View,
+   Image,
+   TouchableHighlight,
+   Dimensions,
+   Picker,
+   Alert,
+   DeviceEventEmitter,
 } from 'react-native';
 
 import Translation from './Translation';
@@ -20,46 +20,45 @@ let pic=require('../img/deconnexion.png');
 //Button appearing in the navigationBar when logged in
 export default class LogOutButton extends Component {
 
-  render() {
+   render() {
       return (
-				<View>
-					<TouchableHighlight underlayColor="#F2F2F2" style={styles.touchableButton}onPress={this.onPressDeconnexion}>
-						<Image source={pic} style={styles.icone}/>
-					</TouchableHighlight>
-				</View>
+         <View>
+            <TouchableHighlight underlayColor="#F2F2F2" style={styles.touchableButton}onPress={this.onPressDeconnexion}>
+               <Image source={pic} style={styles.icone}/>
+            </TouchableHighlight>
+         </View>
       );
-    }
+   }
 
-	// a pop up appears when the user wants to log out from his account asking him if he is sure
-    onPressDeconnexion = () => {
-		Alert.alert(
-		Translation[lang].deconnexion,
-			Translation[lang].deconnexion_text,
-			[
-				{text:Translation[lang].oui,onPress: () => {connection=0;DeviceEventEmitter.emit('nvBar');}},
-				{text:Translation[lang].non,onPress: () => {}},
-			],
-			)
-  	};
- }
+   // a pop up appears when the user wants to log out from his account asking him if he is sure
+   onPressDeconnexion = () => {
+      Alert.alert(
+         Translation[lang].deconnexion,
+         Translation[lang].deconnexion_text,
+         [
+            {text:Translation[lang].oui,onPress: () => {connection=0;DeviceEventEmitter.emit('nvBar');}},
+            {text:Translation[lang].non,onPress: () => {}},
+         ],
+      )
+   };
+}
 
-  const styles = StyleSheet.create({
-    viewContainer:{
+const styles = StyleSheet.create({
+   viewContainer:{
       width:width*0.15,
-	  height:height*0.08,
-    },
-    picker:{
-      width:width*0.07,
-    },
-	icone:{
-      width: width*0.08,
-      height: height*0.045,
-    },
-	touchableButton:{
-	  marginTop: 9,
-	  marginRight: 5,
-      width: width*0.11,
-      height: height*0.06,
-	  
-	}
+      height:height*0.08,
+   },
+   picker:{
+      width:width*0.09,
+   },
+   icone:{
+      width: width*0.10,
+      height: height*0.05,
+   },
+   touchableButton:{
+      marginTop: 8,
+      marginRight:9,
+      width: width*0.10,
+      height: height*0.05,
+   }
 });
